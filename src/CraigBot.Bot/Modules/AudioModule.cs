f0f7ce11c4e60825740e0f5642c90ebae3462553
George Blackwell <geo.wmb@gmail.com>
2020-10-28 20:12:18 +0000

    added more inspect commands, added beginnings of audio module and added latency command

---
+++ src/CraigBot.Bot/Modules/AudioModule.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using CraigBot.Bot.Common;
+using Discord;
+using Discord.Commands;
+
+namespace CraigBot.Bot.Modules
+{
+    // TODO: Finish implementing this
+    
+    [Summary("Audio Commands")]
+    [RequireContext(ContextType.Guild)]
+    public class AudioModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("join", RunMode = RunMode.Async)]
+        [Summary("Makes the Bot join your current voice channel.")]
+        [Example("!join")]
+        public async Task Join()
+        {
+            var channel = (Context.User as IGuildUser)?.VoiceChannel;
+
+            if (channel == null)
+            {
+                await ReplyAsync("You have to be in a voice channel for me to join!");
+                return;
+            }
+            
+            await channel.ConnectAsync();
+        }
+        
+        [Command("leave")]
+        [Summary("Makes the Bot leave its current voice channel.")]
+        [Example("!leave")]
+        public async Task Leave()
+        {
+            var channel = (Context.Client.GetUser(Context.Client.CurrentUser.Id) as IGuildUser)?.VoiceChannel;
+
+            if (channel == null)
+            {
+                await ReplyAsync("I'm not currently in a voice channel.");
+                return;
+            }
+            
+            await channel.DisconnectAsync();
+        }
+    }
+}
