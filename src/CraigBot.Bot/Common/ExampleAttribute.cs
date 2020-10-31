038045787b5fbac2cc8175777c5c6ef7f3af7961
George Blackwell <geo.wmb@gmail.com>
2020-10-31 18:59:36 +0000

    updated configs, help command improvements and other small fixes

--- src/CraigBot.Bot/Common/ExampleAttribute.cs
+++ src/CraigBot.Bot/Common/ExampleAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace CraigBot.Bot.Common
 {
+    // TODO: Allow the prefix to be passed in maybe?
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ExampleAttribute : Attribute
     {
