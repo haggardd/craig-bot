0e3e5167ec6134ec0cc06958a73297b1f922f8b6
George Blackwell <geo.wmb@gmail.com>
2021-02-04 19:18:55 +0000

    fraction & betting module tweaks

--- src/CraigBot.Bot/Common/Fraction.cs
+++ src/CraigBot.Bot/Common/Fraction.cs
@@ -38,6 +38,6 @@
             return true;
         }
         
-        private static readonly char[] DelimiterChars = { '/', '\\', '|', ':', '.' };
+        private static readonly char[] DelimiterChars = { '/', '\\', '|' };
     }
 }
